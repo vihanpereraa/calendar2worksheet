d475189ff2f92ece1d43af3d66ab6b192a756b55
vihan0 <vihanperera334@gmail.com>
2023-01-26 15:50:56 -0500

    version1

--- app.py
+++ app.py
@@ -1,1 +1,37 @@
-print("shutthefuckup")
+function getEvents(){
+
+
+var ss = SpreadsheetApp.getActiveSpreadsheet();
+var sheet = ss.getSheetByName("Mainv2");
+
+
+var calendar = CalendarApp.getCalendarById("vihanperera334@gmail.com");
+var events = calendar.getEvents(new Date("1/20/2023 12:00 AM"), new Date("12/30/2023 12:00 AM" ));
+
+for(var i = 0;i<events.length;i++){
+  var title = events[i].getTitle();
+  var start_time = events[i].getStartTime();
+  var end_time = events[i].getEndTime();
+  var location = events[i].getLocation();
+
+sheet.getRange(i+2,1).setValue(title)
+sheet.getRange(i+2,2).setValue(start_time)
+sheet.getRange(i+2,3).setValue(end_time)
+sheet.getRange(i+2,4).setValue(location)
+
+Logger.log("Events added.");
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+}
